c15b32a0aeaac9cc1f292ec11c9da89b0ed82eaf
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-08-08 15:16:09 +0100

    Update the function taht change dots color

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import SidiBouSaid from './assets/img/sidi-bou-said.jpg'
 import Sousse from './assets/img/sousse.jpg'
 import Jem from './assets/img/el-jem-tunisia.jpg'
 import Dot from './assets/svg/navigation-dot.svg'
+import Sunset from './assets/img/sunset.jpg'
+import Sea from './assets/img/sea.jpg'
 const imagesContainer = document.querySelector('.images-container')
 const activeImage = document.querySelector('.active-image')
 const nextImgBtn = document.querySelector('.next-img')
@@ -19,35 +21,58 @@ sousseImg.src = Sousse
 const jemImg = new Image()
 jemImg.src = Jem
 
-const slides = [sidiBouSaidImg, jemImg, sousseImg]
-imagesContainer.append(sidiBouSaidImg, sousseImg)
-activeImage.append(slides[0])
+const sunsetImg = new Image()
+sunsetImg.src = Sunset
+
+const seaImg = new Image()
+seaImg.src = Sea
+
+const slides = [sidiBouSaidImg, jemImg, sousseImg, sunsetImg, seaImg]
+imagesContainer.append(sidiBouSaidImg, sousseImg, sunsetImg, seaImg)
 
 function createDot() {
-    const dotSvg = new Image(30, 30)
+    const dotSvg = new Image(15, 15)
     dotSvg.src = Dot
 
     return dotSvg
 }
 
 function addDotForImg() {
-    slides.forEach(() => {
+    slides.forEach((_, imgIndex) => {
         const newDot = createDot()
+        newDot.dataset.dotindex = imgIndex
         navigationDotsContainer.append(newDot)
+        
     })
 }
 addDotForImg()
 
+function changeDotColor(dotIndex) {
+    const dots = navigationDotsContainer.children
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].style.backgroundColor = i === dotIndex ? 'black' : 'transparent'
+    }
+}
+
+function initialState() {
+    activeImage.append(slides[0])
+    changeDotColor(0)
+}
+initialState()  
+
 let index = 0
+
 function nextImg() {
     index++
     for (let i=0; i<slides.length; i++) {
         if (index === slides.length) {
             index = index - slides.length
+           
         }
-        activeImage.replaceChildren(slides[index])        
-        
+        activeImage.replaceChildren(slides[index])                
+        changeDotColor(index)
     }
+
 }
 nextImgBtn.addEventListener('click', nextImg)
 
@@ -58,10 +83,27 @@ function previousImg() {
             index = slides.length - 1
         }
         activeImage.replaceChildren(slides[index])
+        changeDotColor(index)
+        
     }
 }
 previousImgBtn.addEventListener('click', previousImg)
 
-// setInterval(nextImg, 5000)
+function changeImgOnDotClick() {
+    navigationDotsContainer.addEventListener('click', (e) => {
+        if (e.target.dataset.dotindex !== undefined) {
+            let clickedDot = parseInt(e.target.dataset.dotindex)
+            index = clickedDot
+            console.log(index)
+            activeImage.replaceChildren(slides[clickedDot])
+            changeDotColor(index)
+        }
+        
+        
+    })
+}
+changeImgOnDotClick()
+
+// setInterval(nextImg, 3000)
 
 
